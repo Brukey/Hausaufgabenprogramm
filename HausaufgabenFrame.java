3c685646aaef53bddd133a66f0f27cbbf9981243
Brukey <42839066+Brukey@users.noreply.github.com>
2018-09-27 09:38:22 +0200

    Update HausaufgabenFrame.java

--- HausaufgabenFrame.java
+++ HausaufgabenFrame.java
@@ -9,16 +9,16 @@ public class HausaufgabenFrame extends JFrame{
 	JTabbedPane tabbedPane;
 	GleichungPanel gleichungPanel;
 	public HausaufgabenFrame() {
-	this.setSize(640,480);
-	this.setResizable(false);
-	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	this.setLayout(null);
-	
-	tabbedPane=new JTabbedPane();
-	
-	gleichungPanel=new GleichungPanel(this);
-	this.getContentPane().add(gleichungPanel);
-	this.setVisible(true);
+		this.setSize(640,480);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(null);
+
+		tabbedPane=new JTabbedPane();
+
+		gleichungPanel=new GleichungPanel(this);
+		this.getContentPane().add(gleichungPanel);
+		this.setVisible(true);
 	}
 	
 	public static void main(String[] args) {
